8492a77861fb9e130d0a703f997ae81dca27a9c8
Kazeem <97337763+thedoskit@users.noreply.github.com>
2024-11-13 16:39:15 +0100

    Add project files.

---
+++ SimpleProductAPI/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleProductAPI.DataAccess;
+using SimpleProductAPI.Domain.Models;
+
+namespace SimpleProductAPI.Controllers
+{
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductContext _context;
+
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAll()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetById(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+            return product;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> Create(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Product updatedProduct)
+        {
+            if (id != updatedProduct.Id) return BadRequest();
+            _context.Entry(updatedProduct).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Products.Any(p => p.Id == id))
+                    return NotFound();
+                throw;
+            }
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ SimpleProductAPI/DataAccess/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleProductAPI.Domain.Models;
+
+namespace SimpleProductAPI.DataAccess
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
